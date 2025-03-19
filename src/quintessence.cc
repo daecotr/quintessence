4c4349231b2b200e5ff3eb11061791e9ce2ee7c6
daecotr <c.daecotr@gmail.com>
2025-03-19 23:18:26 +0200

    Instance & debug utils messenger creation

--- src/quintessence.cc
+++ src/quintessence.cc
@@ -21,6 +21,69 @@ int main(int argc, char **argv) {
 		if (!window)
 			throw std::runtime_error("Failed to create `Window`");
 
+		vk::ApplicationInfo applicationInfo{"", 0, "", 0, vk::makeApiVersion(0, 1, 3, 296)};
+
+		std::vector<const char *> instanceLayers = {"VK_LAYER_KHRONOS_validation"},
+															instanceExtensions = {vk::EXTDebugUtilsExtensionName};
+
+		const auto availableInstanceLayers = vk::enumerateInstanceLayerProperties();
+		for (const auto &instanceLayer: instanceLayers) {
+			bool instanceLayerFound = false;
+			for (const auto &availableInstanceLayer: availableInstanceLayers) {
+				if (std::strcmp(availableInstanceLayer.layerName, instanceLayer) == 0) {
+					instanceLayerFound = true;
+					break;
+				}
+			}
+			if (!instanceLayerFound)
+				throw std::runtime_error("Failed to find required instance layer " + std::string{instanceLayer});
+		}
+
+		uint32_t glfwInstanceExtensionCount = 0;
+		const char **glfwInstanceExtensions = glfwGetRequiredInstanceExtensions(&glfwInstanceExtensionCount);
+		instanceExtensions.insert(instanceExtensions.end(), glfwInstanceExtensions,
+															glfwInstanceExtensions + glfwInstanceExtensionCount);
+
+		const auto availableInstanceExtensions = vk::enumerateInstanceExtensionProperties();
+		for (const auto &instanceExtension: instanceExtensions) {
+			bool instanceExtensionFound = false;
+			for (const auto &availableInstanceExtension: availableInstanceExtensions) {
+				if (std::strcmp(availableInstanceExtension.extensionName, instanceExtension) == 0) {
+					instanceExtensionFound = true;
+					break;
+				}
+			}
+			if (!instanceExtensionFound)
+				throw std::runtime_error("Failed to find required instance extension " + std::string{instanceExtension});
+		}
+
+		const vk::InstanceCreateInfo instanceCreateInfo{vk::InstanceCreateFlags{},
+																										&applicationInfo,
+																										static_cast<uint32_t>(instanceLayers.size()),
+																										instanceLayers.data(),
+																										static_cast<uint32_t>(instanceExtensions.size()),
+																										instanceExtensions.data()};
+
+		vk::UniqueInstance instance = vk::createInstanceUnique(instanceCreateInfo);
+
+		constexpr vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo{
+				{},
+				vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose | vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo |
+						vk::DebugUtilsMessageSeverityFlagBitsEXT::eError | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning,
+				vk::DebugUtilsMessageTypeFlagBitsEXT::eDeviceAddressBinding | vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+						vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation,
+				[](VkDebugUtilsMessageSeverityFlagBitsEXT, VkDebugUtilsMessageTypeFlagsEXT,
+					 const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *) VKAPI_ATTR -> vk::Bool32 {
+					std::cout << pCallbackData->pMessage << std::endl;
+					return vk::False;
+				}};
+
+		const vk::DispatchLoaderDynamic dispatchLoaderDynamic{*instance, vkGetInstanceProcAddr};
+		auto debugUtilsMessenger =
+				instance->createDebugUtilsMessengerEXTUnique(debugUtilsMessengerCreateInfo, nullptr, dispatchLoaderDynamic);
+
+		const std::vector<const char *> deviceExtensions = {vk::KHRSwapchainExtensionName};
+
 		glfwPollEvents();
 
 		glfwDestroyWindow(window);
