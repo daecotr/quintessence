989994797d1b00f9229735042c099877cbeef57f
daecotr <c.daecotr@gmail.com>
2025-03-20 09:56:50 +0200

    Split logic into functions

--- src/quintessence.cc
+++ src/quintessence.cc
@@ -1,3 +1,4 @@
+#include <concepts>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
@@ -5,11 +6,51 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <functional>
 #include <memory>
 #include <vulkan/vulkan.hpp>
 
-int main(int argc, char **argv) {
-  try {
+namespace Q {
+
+template <class TClass> class Singleton {
+public:
+  Singleton(const Singleton &) = delete;
+  Singleton &operator=(const Singleton &) = delete;
+
+  static TClass &getInstance();
+
+protected:
+  Singleton() = default;
+  ~Singleton() = default;
+
+private:
+  struct Deleter {
+    void operator()(TClass *ptr) const { delete ptr; }
+  };
+
+  using UniqueTClass = std::unique_ptr<TClass, Deleter>;
+
+  static UniqueTClass &getStorage();
+};
+
+template <class TClass> TClass &Singleton<TClass>::getInstance() {
+  UniqueTClass &storage = getStorage();
+  if (!storage)
+    storage.reset(new TClass());
+  return *storage;
+}
+
+template <class TClass>
+typename Singleton<TClass>::UniqueTClass &Singleton<TClass>::getStorage() {
+  static UniqueTClass storage;
+  return storage;
+}
+
+class WindowManager : public Singleton<WindowManager> {
+  friend class Singleton<WindowManager>;
+
+protected:
+  WindowManager() {
     glfwSetErrorCallback([](int, const char *description) {
       throw std::runtime_error(description);
     });
@@ -19,94 +60,174 @@ int main(int argc, char **argv) {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    GLFWwindow *window = glfwCreateWindow(512, 512, "", nullptr, nullptr);
-    if (!window)
-      throw std::runtime_error("Failed to create `Window`");
+    std::cout << "WindowManager initialized" << std::endl;
+  }
 
-    vk::ApplicationInfo applicationInfo{"", 0, "", 0,
-                                        vk::makeApiVersion(0, 1, 3, 296)};
+  ~WindowManager() {
+    glfwTerminate();
+    std::cout << "WindowManager destroyed" << std::endl;
+  }
+};
 
-    std::vector<const char *> instanceLayers = {"VK_LAYER_KHRONOS_validation"},
-                              instanceExtensions = {
-                                  vk::EXTDebugUtilsExtensionName};
+std::string getNameOfEnumerated(const vk::LayerProperties &layer) {
+  return layer.layerName;
+}
 
-    const auto availableInstanceLayers = vk::enumerateInstanceLayerProperties();
-    for (const auto &instanceLayer : instanceLayers) {
-      bool instanceLayerFound = false;
-      for (const auto &availableInstanceLayer : availableInstanceLayers) {
-        if (std::strcmp(availableInstanceLayer.layerName, instanceLayer) == 0) {
-          instanceLayerFound = true;
-          break;
-        }
+std::string getNameOfEnumerated(const vk::ExtensionProperties &extension) {
+  return extension.extensionName;
+}
+
+template <typename TAvailableProperties>
+concept AvailableProperties =
+    std::ranges::input_range<TAvailableProperties> &&
+    requires(std::ranges::range_reference_t<TAvailableProperties>
+                 availableProperty) {
+      {
+        getNameOfEnumerated(availableProperty)
+      } -> std::convertible_to<std::string>;
+    };
+
+template <typename TAvailableProperties>
+void checkAvailable(const std::vector<const char *> &names,
+                    const TAvailableProperties &availableProperties) {
+  for (const auto &name : names) {
+    bool found = false;
+    for (const auto &availableProperty : availableProperties) {
+      if (std::strcmp(getNameOfEnumerated(availableProperty).c_str(), name) ==
+          0) {
+        found = true;
+        break;
       }
-      if (!instanceLayerFound)
-        throw std::runtime_error("Failed to find required instance layer " +
-                                 std::string{instanceLayer});
     }
+    if (!found)
+      throw std::runtime_error(
+          "Failed to find required instance layer/extension " +
+          std::string{name});
+  }
+}
 
-    uint32_t glfwInstanceExtensionCount = 0;
-    const char **glfwInstanceExtensions =
-        glfwGetRequiredInstanceExtensions(&glfwInstanceExtensionCount);
-    instanceExtensions.insert(instanceExtensions.end(), glfwInstanceExtensions,
-                              glfwInstanceExtensions +
-                                  glfwInstanceExtensionCount);
+void checkLayersAvailable(const std::vector<const char *> &names) {
+  checkAvailable(names, vk::enumerateInstanceLayerProperties());
+}
 
-    const auto availableInstanceExtensions =
-        vk::enumerateInstanceExtensionProperties();
-    for (const auto &instanceExtension : instanceExtensions) {
-      bool instanceExtensionFound = false;
-      for (const auto &availableInstanceExtension :
-           availableInstanceExtensions) {
-        if (std::strcmp(availableInstanceExtension.extensionName,
-                        instanceExtension) == 0) {
-          instanceExtensionFound = true;
-          break;
-        }
-      }
-      if (!instanceExtensionFound)
-        throw std::runtime_error("Failed to find required instance extension " +
-                                 std::string{instanceExtension});
-    }
+void checkExtensionsAvailable(const std::vector<const char *> &names) {
+  checkAvailable(names, vk::enumerateInstanceExtensionProperties());
+}
 
-    const vk::InstanceCreateInfo instanceCreateInfo{
-        vk::InstanceCreateFlags{},
-        &applicationInfo,
-        static_cast<uint32_t>(instanceLayers.size()),
-        instanceLayers.data(),
-        static_cast<uint32_t>(instanceExtensions.size()),
-        instanceExtensions.data()};
+void UniqueWindowDestroy(GLFWwindow *window) {
+  glfwDestroyWindow(window);
+  std::cout << "Window destroyed" << std::endl;
+}
 
-    vk::UniqueInstance instance = vk::createInstanceUnique(instanceCreateInfo);
+using UniqueWindow =
+    std::unique_ptr<GLFWwindow, decltype(&UniqueWindowDestroy)>;
+UniqueWindow UniqueWindowCreate(int width, int height, const char *title = "") {
+  GLFWwindow *rawWindow =
+      glfwCreateWindow(width, height, title, nullptr, nullptr);
+  if (!rawWindow)
+    throw std::runtime_error("Failed to create `Window`");
+  std::cout << "Window created" << std::endl;
+  return {rawWindow, UniqueWindowDestroy};
+};
 
-    constexpr vk::DebugUtilsMessengerCreateInfoEXT
-        debugUtilsMessengerCreateInfo{
-            {},
-            vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
-                vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo |
-                vk::DebugUtilsMessageSeverityFlagBitsEXT::eError |
-                vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning,
-            vk::DebugUtilsMessageTypeFlagBitsEXT::eDeviceAddressBinding |
-                vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
-                vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
-                vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation,
-            [](VkDebugUtilsMessageSeverityFlagBitsEXT,
-               VkDebugUtilsMessageTypeFlagsEXT,
-               const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-               void *) VKAPI_ATTR -> vk::Bool32 {
-              std::cout << pCallbackData->pMessage << std::endl;
-              return vk::False;
-            }};
+std::vector<const char *> getInstanceLayers() {
+  std::vector<const char *> instanceLayers = {"VK_LAYER_KHRONOS_validation"};
+  checkLayersAvailable(instanceLayers);
+  return instanceLayers;
+}
 
-    const vk::DispatchLoaderDynamic dispatchLoaderDynamic{
-        *instance, vkGetInstanceProcAddr};
-    auto debugUtilsMessenger = instance->createDebugUtilsMessengerEXTUnique(
-        debugUtilsMessengerCreateInfo, nullptr, dispatchLoaderDynamic);
+std::vector<const char *> getInstanceExtensions() {
+  std::vector<const char *> instanceExtensions = {
+      vk::EXTDebugUtilsExtensionName};
+  checkExtensionsAvailable(instanceExtensions);
 
-    VkSurfaceKHR rawSurface;
-    if (glfwCreateWindowSurface(*instance, window, nullptr, &rawSurface) !=
-        VK_SUCCESS)
-      throw std::runtime_error("Failed to create window surface");
-    const vk::SurfaceKHR surface = rawSurface;
+  uint32_t glfwInstanceExtensionCount = 0;
+  const char **glfwInstanceExtensions =
+      glfwGetRequiredInstanceExtensions(&glfwInstanceExtensionCount);
+  instanceExtensions.insert(instanceExtensions.end(), glfwInstanceExtensions,
+                            glfwInstanceExtensions +
+                                glfwInstanceExtensionCount);
+
+  return instanceExtensions;
+}
+
+vk::UniqueInstance createInstance() {
+  vk::ApplicationInfo applicationInfo{"", 0, "", 0,
+                                      vk::makeApiVersion(0, 1, 3, 296)};
+
+  std::vector<const char *> instanceLayers = getInstanceLayers(),
+                            instanceExtensions = getInstanceExtensions();
+
+  const vk::InstanceCreateInfo instanceCreateInfo{
+      vk::InstanceCreateFlags{},
+      &applicationInfo,
+      static_cast<uint32_t>(instanceLayers.size()),
+      instanceLayers.data(),
+      static_cast<uint32_t>(instanceExtensions.size()),
+      instanceExtensions.data()};
+
+  return vk::createInstanceUnique(instanceCreateInfo);
+}
+
+vk::UniqueHandle<vk::DebugUtilsMessengerEXT, vk::DispatchLoaderDynamic>
+createDebugUtilsMessenger(vk::UniqueInstance &instance,
+    const vk::DispatchLoaderDynamic &dispatchLoaderDynamic) {
+  constexpr vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo{
+      {},
+      vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+          vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo |
+          vk::DebugUtilsMessageSeverityFlagBitsEXT::eError |
+          vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning,
+      vk::DebugUtilsMessageTypeFlagBitsEXT::eDeviceAddressBinding |
+          vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+          vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+          vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation,
+      [](VkDebugUtilsMessageSeverityFlagBitsEXT,
+         VkDebugUtilsMessageTypeFlagsEXT,
+         const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *)
+          VKAPI_ATTR -> vk::Bool32 {
+            std::cout << pCallbackData->pMessage << std::endl;
+            return vk::False;
+          }};
+
+  return instance->createDebugUtilsMessengerEXTUnique(
+      debugUtilsMessengerCreateInfo, nullptr, dispatchLoaderDynamic);
+}
+} // namespace Q
+
+int main(int argc, char **argv) {
+  try {
+    Q::WindowManager::getInstance();
+    Q::UniqueWindow window{Q::UniqueWindowCreate(512, 512)};
+
+    auto instance = Q::createInstance();
+    const vk::DispatchLoaderDynamic dispatchLoaderDynamic{*instance,
+                                                      vkGetInstanceProcAddr};
+    auto debugUtilsMessenger = Q::createDebugUtilsMessenger(instance, dispatchLoaderDynamic);
+
+    auto UniqueSurfaceDestroy =
+        [&instance](const vk::SurfaceKHR *surfaceBeingDestroyed) {
+          instance->destroySurfaceKHR(*surfaceBeingDestroyed);
+          std::cout << "Surface destroyed" << std::endl;
+        };
+
+    using UniqueSurface =
+        std::unique_ptr<vk::SurfaceKHR, decltype(UniqueSurfaceDestroy)>;
+
+    auto UniqueSurfaceCreate = [&UniqueSurfaceDestroy, &instance,
+                                &window]() -> UniqueSurface {
+      VkSurfaceKHR rawSurface;
+      if (glfwCreateWindowSurface(*instance, window.get(), nullptr,
+                                  &rawSurface) != VK_SUCCESS)
+        throw std::runtime_error("Failed to create window surface");
+      // ReSharper disable once CppDFAMemoryLeak (UniqueSurfaceDestroy release
+      // memory)
+      const auto surface = new vk::SurfaceKHR{rawSurface};
+      std::cout << "Surface created" << std::endl;
+      return UniqueSurface{surface, UniqueSurfaceDestroy};
+    };
+
+    UniqueSurface surface{UniqueSurfaceCreate()};
 
     const auto availableDevices = instance->enumeratePhysicalDevices();
     if (availableDevices.empty())
@@ -117,10 +238,6 @@ int main(int argc, char **argv) {
 
     glfwPollEvents();
 
-    instance->destroySurfaceKHR(surface);
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
   } catch (const std::exception &exception) {
     std::cerr << exception.what() << std::endl;
     return EXIT_FAILURE;
